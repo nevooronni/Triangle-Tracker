af9c94408c54bf20f1bb061c2500edd569320c81
Neville Oronni <nevooronni@gmail.com>
2017-08-21 01:25:19 +0300

    added back-end functionality

--- js/script.js
+++ js/script.js
@@ -4,26 +4,36 @@ $(document).ready(function() {
 
 //back-end
 var triangle = function(side1, side2, side3) {
-	if((side1 === 0) | (side2 === 0) | (side3 === 0)) {
-		alert("Number entered should start from 1 and above!");
-	} else if(side1 === side2 === side3) {
-		$(".replace").slideToggle();
-	} else if((side1 === side2) | (side1 === side3) | (side2 === side3)) {
-		$("#replace2").slideToggle();
-	} else if((side1 !== side2) | (side2 !== side3)) {
-		$("#replace3").slideToggle();
-	} else {
-		alert("please enter a number in all the spaces!");
+	if(((side1 + side2) >= side3) && ((side2 + side3) >= side1) && ((side3 + side1) && side2)) { 
+		return typeOfTriangle(side1, side2, side3);//noticed that this return is very key in my code
+  } else {
+		$("#replace4").slideToggle();//no triangle
 	}
-}	
+};
+
+var typeOfTriangle = function(side1, side2, side3) {
+	if((side1 === side2) && (side2 === side3)) {
+		return $(".replace").slideToggle();//equilateral triangle
+	} else if((side1 === side2) || (side1 === side3) || (side2 === side3)) {
+		return $("#replace2").slideToggle();//isoceles triangle
+	} else {
+		return $("#replace3").slideToggle();//scalene triangle
+	}
+};
 
 //front-end
-	$("button.formbtn").click(function(event) {
+	$("input.formbtn").click(function(event) {
 		event.preventDefault();
 		var side1 = parseInt($("#side1").val());
 		var side2 = parseInt($("#side2").val());
 		var side3 = parseInt($("#side3").val());
 		var show = triangle(side1, side2, side3);
-		$().slideToggle(show);
+		show;
+	});
+	$(".newbtn").click(function() {
+		location.reload();
 	});
 });
+
+
+
